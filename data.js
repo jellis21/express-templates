2732e62dbc9289749ba9a724f5e4ba3d3c717c34
Joshua Ellis <joshua.ellis.r@gmail.com>
2021-10-06 09:34:01 -0400

    creates partials head and foot

---
+++ data.js
@@ -0,0 +1,21 @@
+// this data will act as our database
+const data = [
+  {
+    name: "Blueberry Pancakes",
+    handle: "bluepan"
+  },
+  {
+    name: "Chicken & Waffles",
+    handle: "wafflechick"
+  }, 
+  {
+    name: "French Toast",
+    handle: "ftoast",
+  },
+  {
+    name: "Shrimp & Grits",
+    handle: "s&g"
+  }
+]
+
+module.exports = data;
